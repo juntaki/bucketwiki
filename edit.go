1ad505aadda40cd025b842235a04e08320497427
juntaki <me@juntaki.com>
2016-11-09 00:20:06 +0900

    Move edit related function

---
+++ edit.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func editfunc(c *gin.Context) {
+	s3 := c.MustGet("S3").(*Wikidata)
+	titleHash := c.Param("titleHash")
+	title := c.Query("title")
+	body := "# " + title + "\n"
+	markdown, err := s3.loadMarkdown(titleHash)
+	if err == nil {
+		body = markdown.body
+	}
+	c.HTML(http.StatusOK, "edit.html", gin.H{
+		"Title":     title,
+		"TitleHash": titleHash,
+		"Body":      body,
+	})
+}
+
+func uploadfunc(c *gin.Context) {
+	s3 := c.MustGet("S3").(*Wikidata)
+	titleHash := c.Param("titleHash")
+	page, err := s3.loadMarkdownMetadata(titleHash)
+	if err != nil {
+		fmt.Println(err)
+	}
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		fmt.Println(err)
+	}
+	filename := header.Filename
+	contentType := header.Header["Content-Type"][0]
+
+	s3.saveFile(page, file, filename, contentType)
+}
+
+func postfunc(c *gin.Context) {
+	method, _ := c.GetPostForm("_method")
+	fmt.Println(method)
+	switch method {
+	case "put":
+		putfunc(c)
+	case "delete":
+		deletefunc(c)
+	}
+}
+
+func deletefunc(c *gin.Context) {
+	s3 := c.MustGet("S3").(*Wikidata)
+	titleHash := c.Param("titleHash")
+	s3.deleteMarkdown(titleHash)
+	s3.deleteHTML(titleHash)
+	c.Redirect(http.StatusFound, "/")
+}
+
+func putfunc(c *gin.Context) {
+	s3 := c.MustGet("S3").(*Wikidata)
+	titleHash := c.Param("titleHash")
+	title, _ := c.GetPostForm("title")
+	if titleHash != s3.titleHash(title) {
+		fmt.Println("title not match")
+		fmt.Println("title:", title)
+		fmt.Println("generated:", s3.titleHash(title))
+		fmt.Println("titleHash:", titleHash)
+		c.Redirect(http.StatusFound, "/500")
+		return
+	}
+
+	session := sessions.Default(c)
+	user := session.Get("user")
+
+	var markdown, html pageData
+
+	markdown.titleHash = titleHash
+	markdown.title = title
+	markdown.author = user.(string)
+	markdown.body, _ = c.GetPostForm("body")
+	err := s3.saveMarkdown(markdown)
+	if err != nil {
+		fmt.Println("save Markdown", err)
+		c.Redirect(http.StatusFound, "/500")
+		return
+	}
+
+	html.titleHash = titleHash
+	html.title = title
+	html.author = user.(string)
+	html.body, _ = MarkdownToHTML(s3, []byte(markdown.body))
+	err = s3.saveHTML(html)
+	if err != nil {
+		fmt.Println("save HTML", err)
+		c.Redirect(http.StatusFound, "/500")
+		return
+	}
+
+	c.Redirect(http.StatusFound, "/page/"+titleHash)
+}
